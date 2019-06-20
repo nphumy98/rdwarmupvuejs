8b32580ffa58faad0a9d1c660b862af03c9e30ff
Jimmy Nguyen <nphumy89@gmail.com>
2019-06-20 17:14:19 +1200

    refactor but keep commenting

--- src/main.js
+++ src/main.js
@@ -1,32 +1,37 @@
 import Vue from 'vue'
-import Header from './components/Header.vue'
-import LeftPanel from './components/LeftPanel.vue'
-import RightPanel from './components/RightPanel.vue'
-import axios from 'axios'
+import App from './App.vue'
+// import Header from './components/Header.vue'
+// import LeftPanel from './components/LeftPanel.vue'
+// import RightPanel from './components/RightPanel.vue'
+// import axios from 'axios'
 
 Vue.config.productionTip = false
 
-Vue.component('header-comp',Header);
-Vue.component('left-panel',LeftPanel);
-Vue.component('right-panel',RightPanel);
+// Vue.component('header-comp',Header);
+// Vue.component('left-panel',LeftPanel);
+// Vue.component('right-panel',RightPanel);
 
 new Vue({
-  el: '.container',
-  data () {
-   return {
-     info: null
-   }
- },
-  mounted () {
-    axios
-      .get('http://localhost:3000/companies')
-      .then((response) => {
-        console.log(response.data);
-        this.info = response.data;
-      })
-      .catch((error) => {
-        console.log(error);
-        this.info = "Connect to Sever Fail"
-      })
-  }
-})
+  render: h => h(App),
+}).$mount('#app')
+
+// new Vue({
+//   el: '.container',
+//   data () {
+//    return {
+//      info: null
+//    }
+//  },
+//   mounted () {
+//     axios
+//       .get('http://localhost:3000/companies')
+//       .then((response) => {
+//         console.log(response.data);
+//         this.info = response.data;
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//         this.info = "Connect to Sever Fail"
+//       })
+//   }
+// })
