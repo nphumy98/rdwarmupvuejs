1e2f854d69f4d9200a0ada7544a95c99d3a86054
Jimmy Nguyen <nphumy89@gmail.com>
2019-06-20 16:43:36 +1200

    add axios and connect to sever

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Header from './components/Header.vue'
 import LeftPanel from './components/LeftPanel.vue'
 import RightPanel from './components/RightPanel.vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -10,5 +11,15 @@ Vue.component('left-panel',LeftPanel);
 Vue.component('right-panel',RightPanel);
 
 new Vue({
-  el: '.container'
+  el: '.container',
+  data () {
+   return {
+     info: null
+   }
+ },
+  mounted () {
+    axios
+      .get('http://localhost:3000/companies')
+      .then(response => (this.info = response))
+  }
 })
